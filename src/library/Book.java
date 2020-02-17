c75e59fedfd79f5cb82c7b6e54b457a09f6e1701
Mateusz Dec <mateusz.dec92@gmail.com>
2020-02-17 21:51:44 +0100

    Stworzenie konstruktora i metody wyświetlającej printInfo

--- src/library/Book.java
+++ src/library/Book.java
@@ -17,6 +17,14 @@ public class Book {
         isbn = bookIsbn;
     }
 
+    Book(String bookTitle, String bookAuthor, int bookReleaseDate, int bookPages, String bookPublisher) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseDate = bookReleaseDate;
+        pages = bookPages;
+        publisher = bookPublisher;
+    }
+
     public void printInfo() {
         String info = title + "; " + author + "; " + releaseDate + "; " + pages + "; " + publisher + "; " + isbn;
         System.out.println(info);
