ef97e7dab05e9169ecaa1eff91a3d6140d290ac0
Mateusz Dec <mateusz.dec92@gmail.com>
2020-04-06 19:30:37 +0200

    zmiana metody printInfo

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -4,7 +4,7 @@ import library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.6";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9788373271890");
