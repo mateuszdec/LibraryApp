fc4a08c23af6d4b84e5c17c80348f8b69eab2939
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-04 08:03:00 +0200

    usuwanie ksiązki i magazynu

--- src/library/app/LibraryControl.java
+++ src/library/app/LibraryControl.java
@@ -55,6 +55,12 @@ public class LibraryControl {
                 case PRINT_MAGAZINES:
                     printMagazines();
                     break;
+                case DELETE_BOOK:
+                    deleteBook();
+                    break;
+                case DELETE_MAGAZINE:
+                    deleteMagazine();
+                    break;
                 case EXIT:
                     exit();
                     break;
@@ -98,6 +104,18 @@ public class LibraryControl {
         }
     }
 
+    private void deleteBook() {
+        try {
+            Book book = dataReader.readAndCreateBook();
+            if (library.removePublication(book))
+                printer.printLine("Usunięto książkę");
+            else
+                printer.printLine("Brak wskazanej książki");
+        } catch (InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć książki, niepoprawne dane");
+        }
+    }
+
     private void printBooks() {
         Publication[] publications = library.getPublications();
         printer.printBooks(publications);
@@ -111,6 +129,18 @@ public class LibraryControl {
             printer.printLine("Nie udało się utworzyć magazynu, niepoprawne dane.");
         } catch (ArrayIndexOutOfBoundsException e) {
             printer.printLine("Osiągnięto limit pojemności, nie można dodać kolejnego magazynu");
+        }
+    }
+
+    private void deleteMagazine() {
+        try {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        if (library.removePublication(magazine))
+            printer.printLine("Usunięto magazyn");
+        else
+            printer.printLine("Brak wskazanego magazynu");
+        } catch (InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć magazynu, niepoprawne dane");
         }
     }
 
@@ -136,7 +166,9 @@ public class LibraryControl {
         ADD_BOOK(1, "Dodanie książki"),
         ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
         PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
-        PRINT_MAGAZINES(4, "WYświetlenie dostępnych magazynów/gazet");
+        PRINT_MAGAZINES(4, "WYświetlenie dostępnych magazynów/gazet"),
+        DELETE_BOOK(5, "Usuś książkę"),
+        DELETE_MAGAZINE(6, "Usuń magazyn");
 
         private int value;
         private String description;
