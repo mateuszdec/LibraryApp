5fca0ef0ccbb6d241dab52c719cac7295f7c3daa
Mateusz Dec <mateusz.dec92@gmail.com>
2020-06-08 11:13:53 +0200

    dodawanie użytkowników

--- src/library/model/User.java
+++ src/library/model/User.java
@@ -3,7 +3,7 @@ package library.model;
 import java.io.Serializable;
 import java.util.Objects;
 
-abstract class User implements Serializable {
+abstract class User implements Serializable, CsvConvertible {
     private String firstName;
     private String lastName;
     private String pesel;
@@ -37,8 +37,6 @@ abstract class User implements Serializable {
     public void setPesel(String pesel) {
         this.pesel = pesel;
     }
-
-    public abstract String toCsv();
 
     @Override
     public boolean equals(Object o) {
